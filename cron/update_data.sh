459e654a196cc48dffd9ca9e23b762fa7c1fdcbd
jrbgit <joanas@gmail.com>
2022-11-18 12:59:46 -0500

    crons

--- cron/update_data.sh
+++ cron/update_data.sh
@@ -1,25 +1,40 @@
-DIR="H:\Projects\GraphLinq\GraphLinq.ScrapperAPI"
-OUT="H:\Projects\GraphLinq\GraphLinq.ScrapperAPI\data"
+#!/bin/bash
 
-cd $DIR source/default-token-list
+echo
+echo "GraphLinq Scraper API Cron Job"
+echo
+
+#!/bin/bash
+
+cd ../data
+
+cd default-token-list
 git pull
 npm run build
-mv build/uniswap-default.tokenlist.json $OUT
+mv build/uniswap-default.tokenlist.json ../uniswap-default.tokenlist.json
+cd ..
 
-cd $DIR source/quickswap-default-token-list
+cd quickswap-default-token-list
 git pull
 npm run build
-mv build/quickswap-default.tokenlist.json $OUT
+mv build/quickswap-default.tokenlist.json ../quickswap-default.tokenlist.json
+cd ..
 
-cd $OUT
+cd tokenlists-org
+ls
+
+cd ..
+
+rm -rf *.json
+
 curl -X 'GET' 'https://api.coingecko.com/api/v3/coins/list?include_platform=true' -H 'accept: application/json' > coingecko.json
 
-cd $OUT
 curl -X 'GET' 'https://wispy-bird-88a7.uniswap.workers.dev/?url=http://tokens.1inch.eth.link' -H 'accept: application/json' > 1inch.json
 
-cd $OUT
 curl -X 'GET' 'https://wispy-bird-88a7.uniswap.workers.dev/?url=http://erc20.cmc.eth.link' -H 'accept: application/json' > cmc200.json
 
-cd $OUT
 curl -X 'GET' 'https://wispy-bird-88a7.uniswap.workers.dev/?url=http://stablecoin.cmc.eth.link' -H 'accept: application/json' > cmcStableCoin.json
 
+echo
+echo "GraphLinq Scraper API Cron Job COMPLETE"
+echo
