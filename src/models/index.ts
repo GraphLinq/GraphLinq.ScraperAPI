9208fe428b125e6801c5009770b1f659ed7535d8
jrbgit <joanas@gmail.com>
2022-11-18 19:27:09 -0500

    No time to commit.. My people need me!

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,6 @@ export * from "./coingecko.model";
 export * from "./quickswap.model";
 export * from "./uniswap.model";
 export * from "./livecoinwatch.model";
-export * from "./coinmarketcap.model";
 export * from "./coinmarketcap200.model";
 export * from "./coinmarketcapstable.model";
 export * from "./pancakeswap.model";
